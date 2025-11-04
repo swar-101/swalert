295a142c56ba8a12843bdca7f6fdc98f920babd5
swar-101 <kunwarswarprakash@gmail.com>
2025-11-04 20:37:17 +0530

    feat: add v0 Electron app

---
+++ electron-app/README.md
@@ -0,0 +1,9 @@
+
+
+`main.js`
+
+- Connects to ntfy topic via WebSocket
+- Listens for messages from your phone
+- Triggers desktop notifications
+- Auto-reconnects if dropped 
+- Minimizes to tray 
---
+++ electron-app/main.js
@@ -0,0 +1,60 @@
+const {app, BrowserWindow, Notification, Tray, Menu} = require('electron');
+const WebSocket = require('ws');
+
+const NTFY_TOPIC = 'swalert-phone-to-pc';
+
+let tray = null;
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 400,
+        height: 300,
+        webPreferences: {preload: __dirname + 'preload.js'},
+    });
+    win.loadFile('renderer/index.html');
+    win.setMenu(null);
+    return win;
+}
+
+function setupTray(win) {
+    tray = new Tray('renderer/icon.png');
+    const menu = Menu.buildFromTemplate([
+        {label : 'Open Swalert', click: () => win.show() },{
+        label : 'Quit', click: () => app.quit()
+        },
+    ]);
+    tray.setToolTip('Swalert Running');
+    tray.setContextMenu(menu);
+}
+
+function startWebSocket() {
+    console.log('Connecting to ntfy...');
+    const ws = new WebSocket(`wss://ntfy.sh/${NTFY_TOPIC}/ws`, {
+        headers: { 'Cache-Control': 'no-cache' }
+    });
+
+    ws.on('message', (data) => {
+        const msg = JSON.parse(data);
+        console.log('Received', msg);
+
+        new Notification({
+            title: msg.title || 'Swalert Incoming 🚨',
+            body: msg.message || msg,
+        }).show();
+    });
+
+    ws.on('close', () => {
+        console.log('Connection closed. Reconnecting in 5s...');
+        setTimeout(startWebSocket, 5000);
+    });
+
+    ws.on('error', (err) => {
+        console.error('Error: ', err.message);
+    });
+}
+
+app.whenReady().then(() => {
+    const win = createWindow();
+    setupTray(win);
+    startWebSocket();
+});
